45e321b9d1f24f0757452114fc81eadb4963826b
tessasouquet <teresouquetroca@gmail.com>
2022-01-06 11:16:14 -0300

    aprendiendo if e else

---
+++ iniciando-javascript/logicos.js
@@ -0,0 +1,11 @@
+const note = 100;
+
+if (note >= 80) {
+  console.log("Parabéns, você foi aprovado!!")
+}
+else if (note < 80 && note >= 60){
+  console.log("Vocẽ entrou na lista de espera")
+}
+else if (note < 60){
+  console.log("Você foi reprovado")
+}
