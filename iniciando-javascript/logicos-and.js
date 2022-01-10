da88d25599197c8430a80a4769305079732660f3
tessasouquet <teresouquetroca@gmail.com>
2022-01-10 11:54:09 -0300

    logico and

---
+++ iniciando-javascript/logicos-and.js
@@ -0,0 +1,20 @@
+const hour = 14;
+let message = "";
+
+if (hour >= 22){
+console.log(message = "Hora de dormir")
+}
+else if (hour >= 18 && hour < 22){
+  console.log(message = "hora de jantar")
+}
+else if (hour >= 14 && hour < 18){
+  console.log(message = "Hora do lanche")
+}
+else if (hour = 11 && hour < 14){
+  console.log(message = "hora do almoço")
+}
+else if (hour = 4 && hour < 11){
+  console.log(message = "hora do cafe da manhã")
+}
+
+console.log(message);
