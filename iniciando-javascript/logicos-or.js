16c3faef413f358587da4e50f58a83eb6f45f8dc
tessasouquet <teresouquetroca@gmail.com>
2022-01-10 12:08:15 -0300

    logico OR

---
+++ iniciando-javascript/logicos-or.js
@@ -0,0 +1,7 @@
+let weekDay = "sabado";
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
+  console.log("Mais um dia de aprendizado")
+}
+else {console.log("Finalmente, descanso!!!")
+}
